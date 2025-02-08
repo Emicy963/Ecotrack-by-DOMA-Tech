89c5162ae53199cec3ea28a86b3ea3f71615ca32
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-02-08 18:25:48 +0100

    ALLOWED_HOSTS config for deploy

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = 'ecotrack-domag-tech.onrender.com'
 
 # Application definition
 
